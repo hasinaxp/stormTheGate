f3ef6b0458b2d70a329f2321ce814549fac3ed3b
Spandan <chocoboyxp@gmail.com>
2019-08-23 00:46:37 +0530

    lost interest

---
+++ models/post.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+let postSchema = new schema({
+    user_id : {type : schema.Types.ObjectId, ref: 'user'},
+    location : {type : String, default:''},
+    caption:  {type : String, default:''},
+    is_text_only: {type: Boolean, default: false},
+    file_array : [{type : String, required: false}],
+    file_type : {type : String, required: true},
+    file_count : {type : Number, default: 0},
+    date: {type: Date, required: true},
+    likes : [{type: schema.Types.ObjectId, ref: 'user'}],
+    answers : [{
+        user_id: {type : schema.Types.ObjectId, ref: 'user'},
+        text : {type: String, default: ''}
+    }],
+    catagory: {type: String, default: ''},
+    is_private: {type: Boolean, default: false}
+
+});
+
+let Post = module.exports = mongoose.model('post', postSchema);
+
+/*
+documentation -- 
+file_type->
+    image
+    video
+*/
